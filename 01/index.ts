49529252819c79313ae2b200642c2f1fd70ce37a
Pavel Lang <langpavel@phpskelet.org>
2025-12-01 08:44:55 +0100

    Day 01 part 2

--- 01/index.ts
+++ 01/index.ts
@@ -6,20 +6,59 @@ const moves = lines.split("\n").map((line) => {
 });
 
 let dialPos = 50;
-let zeroClicks = 0;
+let pointsAtZero = 0;
+let passedZero = 0;
 
 for (const move of moves) {
-  // console.log(`Direction: ${move.direction}, Distance: ${move.distance}`);
+  let currentPassedZero = 0;
+  let currentHitsZero = 0;
+
   if (move.direction === "L") {
+    if (dialPos === 0) {
+      dialPos = 100;
+    }
     dialPos -= move.distance;
+    while (dialPos < 0) {
+      currentPassedZero++;
+      dialPos += 100;
+    }
+    if (dialPos === 0) {
+      currentHitsZero++;
+    }
   } else if (move.direction === "R") {
     dialPos += move.distance;
+    while (dialPos >= 100) {
+      dialPos -= 100;
+      currentPassedZero++;
+    }
+    if (dialPos === 0) {
+      currentHitsZero++;
+      currentPassedZero--;
+    }
   }
-  dialPos = (dialPos + 100) % 100;
-  console.log(`New Dial Position: ${dialPos}`);
-  if (dialPos === 0) {
-    zeroClicks++;
-    console.log(`Zero from Left: ${zeroClicks}`);
+
+  console.log(
+    `${move.direction}${move.distance}\t-> ${dialPos}\t${currentHitsZero}\t${currentPassedZero}`,
+  );
+
+  passedZero += currentPassedZero;
+  pointsAtZero += currentHitsZero;
+
+  const strOut = [
+    `- The dial is rotated ${move.direction}${move.distance} to point at ${dialPos}`,
+  ];
+  if (currentHitsZero) {
+    strOut.push("hits zero");
   }
+  if (currentPassedZero > 0) {
+    strOut.push(
+      `during this rotation, it points at 0 ${
+        currentPassedZero === 1 ? "once" : `${currentPassedZero} times`
+      }`,
+    );
+  }
+
+  // console.log(strOut.join("; "));
 }
-console.log(`Part 1: ${zeroClicks}`);
+console.log(`Part 1: ${pointsAtZero}`);
+console.log(`Part 2: ${passedZero + pointsAtZero}`);
