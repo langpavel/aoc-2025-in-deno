c7ae811f313a057879ff87f28726745b4a0f742f
Pavel Lang <langpavel@phpskelet.org>
2025-12-05 06:29:39 +0100

    Day 04 part 2

--- 04/index.ts
+++ 04/index.ts
@@ -5,7 +5,10 @@ const maxX = lines[0].length;
 const maxY = lines.length;
 
 console.log(`Max X: ${maxX}, Max Y: ${maxY}`);
-console.log(`Lines:\n${lines.join("\n")}`);
+
+const printBoard = () => {
+  console.log(lines.join("\n") + "\n");
+};
 
 const isRollOnPosition = (x: number, y: number): boolean => {
   if (y < 0 || y >= maxY || x < 0 || x >= maxX) {
@@ -13,7 +16,17 @@ const isRollOnPosition = (x: number, y: number): boolean => {
   }
   const line = lines[y];
   const char = line[x];
-  return char === "@";
+  return char === "@" || char === "x";
+};
+
+const markRollOnPosition = (x: number, y: number): void => {
+  lines[y] = lines[y].substring(0, x) + "x" + lines[y].substring(x + 1);
+};
+
+const clearMarked = () => {
+  for (let y = 0; y < maxY; y++) {
+    lines[y] = lines[y].replace(/x/g, ".");
+  }
 };
 
 const isMovableOnPosition = (
@@ -33,13 +46,30 @@ const isMovableOnPosition = (
   return count <= 4;
 };
 
-let result1 = 0;
-for (let y = 0; y < maxY; y++) {
-  for (let x = 0; x < maxX; x++) {
-    if (isRollOnPosition(x, y) && isMovableOnPosition(x, y)) {
-      result1++;
+let wasMarked = false;
+let result2 = 0;
+while (!wasMarked) {
+  wasMarked = false;
+  let result1 = 0;
+  for (let y = 0; y < maxY; y++) {
+    for (let x = 0; x < maxX; x++) {
+      if (isRollOnPosition(x, y) && isMovableOnPosition(x, y)) {
+        result1++;
+        markRollOnPosition(x, y);
+        wasMarked = true;
+      }
     }
   }
+  console.log(`Result 1: ${result1}`);
+  if (result1 === 0) {
+    break;
+  }
+  // printBoard();
+  if (wasMarked) {
+    clearMarked();
+    wasMarked = false;
+  }
+  result2 += result1;
 }
 
-console.log(`Result 1: ${result1}`);
+console.log(`Result 2: ${result2}`);
