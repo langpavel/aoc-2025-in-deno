15552bf00d983887acad0521550efcb77e0da454
Pavel Lang <langpavel@phpskelet.org>
2025-12-11 07:56:12 +0100

    Day 11 part 2

--- 11/part2.ts
+++ 11/part2.ts
@@ -16,10 +16,20 @@ let cacheHits = 0;
 
 const traceAllPaths = (
   from: string,
+  mustPass: Set<string> = new Set(),
   cache: Map<string, number> = new Map(),
 ): number => {
   if (from === "out") {
+    if (mustPass.size > 0) {
+      return 0;
+    }
     return 1;
+  }
+
+  let newMustPass = mustPass;
+  if (mustPass.has(from)) {
+    newMustPass = new Set(mustPass);
+    newMustPass.delete(from);
   }
 
   const targets = devices.get(from);
@@ -27,22 +37,23 @@ const traceAllPaths = (
     throw new Error(`No device found for ${from}`);
   }
 
-  const cacheKey = `${from}`;
+  const cacheKey = `${from}--${[...newMustPass].join("--")}`;
   let result = cache.get(cacheKey);
   if (result !== undefined) {
+    // console.log(`Cache hit for ${cacheKey}`);
     cacheHits++;
     return result;
   }
   result = 0;
   for (const target of targets) {
-    result += traceAllPaths(target, cache);
+    result += traceAllPaths(target, newMustPass, cache);
   }
   cache.set(cacheKey, result);
 
   return result;
 };
 
-console.log(traceAllPaths("you"));
+console.log("svr", traceAllPaths("svr", new Set(["fft", "dac"])));
 
 console.log("Cache hits:", cacheHits);
 console.timeEnd("Day 11, Part 1");
